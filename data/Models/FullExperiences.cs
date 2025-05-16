99a207b78c6321f9175b7be3afd2a30190c2c935
Samuel ISRAEL <samuel.israel@epita.fr>
2025-05-16 19:18:37 +0200

    add affichage experiences

---
+++ data/Models/FullExperiences.cs
@@ -0,0 +1,13 @@
+﻿using CV_front.Models;
+
+namespace CV_front.Models
+{
+    public class FullExperiences
+    {
+        public Experience experiences { get; init; }
+        public List<Competence> competences { get; init; }
+        public List<TacheExperience> taches { get; init; }
+        public string? type { get; init; }
+        
+    }
+}
